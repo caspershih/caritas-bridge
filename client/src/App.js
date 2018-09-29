54839f909a0cdadd3d7cf8fdbf0fdd0ba8523579
Kevin Shipley <35049981+kevin8pizza@users.noreply.github.com>
2018-09-29 13:25:06 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from "./pages/Main";
 import './App.css';
+//import SearchBar from "./components/SearchField/SearchBar";
 
 class App extends Component {
   render() {
