fad6668a4fcdafef0d05a2bda7ce56b92eecbfd9
audrey <audreyfernandez@verizon.net>
2018-09-30 17:24:38 -0700

    added model to create table in MySql for messages

--- models/nonProfit.js
+++ models/nonProfit.js
@@ -1,14 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
   var nonProfit = sequelize.define("nonProfit", {
-    name: {type: DataTypes.STRING, allowNull: false, validate: {len: [1]}},
+    charityName: {type: DataTypes.STRING, allowNull: false, validate: {len: [1]}},
     address1: {type: DataTypes.STRING, allowNull: false},
     address2: {type: DataTypes.STRING, allowNull: false},
     city: {type: DataTypes.STRING, allowNull: false},
     state: {type: DataTypes.STRING, allowNull: false},
     zip: {type: DataTypes.STRING, allowNull: false},
-    category: {type: DataTypes.STRING, allowNull: false},
-    webURL: {type: DataTypes.STRING, allowNull: false},
-    email: {type: DataTypes.STRING, allowNull: false},
+    mission: {type: DataTypes.BLOB, allowNull: true},
+    webURL: {type: DataTypes.STRING, allowNull: true},
+    email: {type: DataTypes.STRING, allowNull: true},
     ein: {type: DataTypes.INTEGER, allowNull: false},
 
 
