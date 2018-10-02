b275eaa4a0ac54ef1db4271e5876c0016104867a
Kevin Shipley <35049981+kevin8pizza@users.noreply.github.com>
2018-10-01 20:38:56 -0700

    merged

--- client/src/components/Search/Results.js
+++ client/src/components/Search/Results.js
@@ -1,47 +1,14 @@
 import React from "react";
 
 const Results = (props) => {
-<<<<<<< HEAD
-  const saveCharity = (event) =>{
-    var index = event.target.name;
-    props.saveCharity(index);
-  }
-=======
   /*const saveCharity = (event) =>{
     var index = event.target.name;
     props.saveCharity(index);
   }*/
->>>>>>> 5c300b33862c451ce40ac1b93f17326fa265a750
 
     return (
 
       <div className="resultsDiv">
-<<<<<<< HEAD
-          <h2>Search Results</h2>
-          <hr />
-        
-            {props.passedResults.map((charity, i) => (
-                <div key={i} id={"result_"+(i+1)} className="well">
-                
-                <h4>Charity Name: {organizations.name}</h4>
-                <span><p>Address: {organizations.address1}</p>
-                <p>{organizations.address2}</p></span>
-                <span><p>{organizations.city}</p>
-                <p>{organizations.state}</p>
-                <p>{organizations.zip}</p></span>
-                <p>Category: {organizations.category}</p>
-                <p>Website: <a href={organizations.webURL} target="_blank" >{organizations.webURL}</a></p>
-                <p>Email Address: {organizations.email}</p>
-                <p>EIN: {organizations.ein}</p>
-                
-                <button name={i} className="btn btn-primary" onClick={saveCharity}> Save Charity</button>
-                
-                </div>
-              ))
-            }
-              
-          </div>
-=======
       <h2>Search Results</h2>
       <hr />
       {this.state.nonProfit.map(nonprofit => 
@@ -58,7 +25,6 @@ const Results = (props) => {
       
       </div>)}
   </div>
->>>>>>> 5c300b33862c451ce40ac1b93f17326fa265a750
     
     );
   
--- client/src/components/Search/Saved.js
+++ client/src/components/Search/Saved.js
@@ -9,28 +9,6 @@ const Saved = (props) => {
     return (
 
 <div className="resultsDiv">
-<<<<<<< HEAD
-          <h2>Saved Charities</h2>
-          <hr />
-        
-            {props.savedCharities.map((data, i) => (
-              <div key={i} id={"result_"+(i+1)} className="well">
-
-                <h4>Charity Name: {data.name}</h4>
-                <span><p>Address: {data.address1}</p>
-                <p>{data.address2}</p></span>
-                <span><p>{data.city}</p>
-                <p>{data.state}</p>
-                <p>{data.zip}</p></span>
-                <p>Category: {data.category}</p>
-                <p>Website: <a href={data.webURL} target="_blank" >{data.webURL}</a></p>
-                <p>Mission: {data.mission}</p>
-                <p>EIN: {data.ein}</p>
-
-                <button name={charity._id} className="btn btn-primary" onClick={removeCharity}> Delete</button> 
-            
-              </div>
-=======
         <h2>Saved Charities</h2>
         <hr />
         {this.state.nonProfit.map(nonprofit => 
@@ -45,7 +23,6 @@ const Saved = (props) => {
           <div className="spacer"><hr /></div>
                     
         </div>)}
->>>>>>> 5c300b33862c451ce40ac1b93f17326fa265a750
               ))
             }              
   </div>
