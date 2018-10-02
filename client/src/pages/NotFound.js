776cc6b572f32b44978fccdaee7e51b9a9b4316b
audrey <audreyfernandez@verizon.net>
2018-10-01 20:23:48 -0700

    404 page update

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./NotFound.css";
+import brokenHeart from "../images/brokenheart404.svg";
 
 const NotFound = () => (
-    <div class="NotFound">404... That page does not exist!</div>
+    
+    <div class="NotFound"><img src={brokenHeart} class="brokenHeart" alt="404 error"></img>
+    <p>404... That page does not exist!</p>
+    </div>
     )
 
 export default NotFound;
