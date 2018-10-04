c096d0219261de766b57ca15b1671e70959d9ddf
Casper Shih <casshih@gmail.com>
2018-10-03 20:10:15 -0700

    trying new things

--- routes/message.js
+++ routes/message.js
@@ -3,52 +3,56 @@ const bodyparser = require("body-parser");
 const nodemailer = require("nodemailer");
 const router = express.Router();
 
-//This app is runnign on express server
+//This function is runnign on express server
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 //The POST route
-app.post("/MessageCenter", (req, res) => {
-    const sendMessage = `
-    <ul>
-        <li> Name: ${req.body.mailto} </li>
-        <li> Subject: ${req.body.subject} </li>
-        <li> Message: ${req.body.message} </li>
-    </ul>
-    `;
+app.post("/api/MessageCenter", (req, res) => {
 
+    nodemailer.createTestAccount((error, account) => {
+        
+        const sendMessage = `
+        <ul>
+            <li> Name: ${req.body.mailto} </li>
+            <li> Subject: ${req.body.subject} </li>
+            <li> Message: ${req.body.message} </li>
+        </ul>
+    `
 //Set up Ethereal email
-    let transporter = nodemailer.createTransport( {
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false,
-        auth: {
-//My ethereal email user and password
-            user: "k2ehxzlx36nubj6o@ethereal.email",
-            pass: "nSgA8PekxPC5Z9jPyb"
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+        let transporter = nodemailer.createTransport( {
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+            
+            //My ethereal email user and password
+                user: "k2ehxzlx36nubj6o@ethereal.email",
+                pass: "nSgA8PekxPC5Z9jPyb"
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
-    //Setup email data with unicode
-    let mailOptions = {
-        from: "Caritas-Bridge <smtp.ethereal.email>",
-        to: "casshih@gmail.com",
-        text: req.body.message,
-        html: sendMessage
-    };
-
+        //Setup email data with unicode
+        let mailOptions = {
+            from: "Caritas-Bridge <smtp.ethereal.email>",
+            to: "casshih@gmail.com",
+            text: req.body.message,
+            html: sendMessage
+        };
+    
     //Send mail with defined transport 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error)
-        }
-        console.log("Message sent: ", info.message);
-        console.log(nodemailer.getTestMessageUrl(info));
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log("Message sent: ", info.message);
+            console.log(nodemailer.getTestMessageUrl(info));
+        });
     });
 });
 
